70561da611a0abed158e82c9570a67b7aa559653
sunshineeleda <thesubjectivesubject@gmail.com>
2019-12-01 23:51:10 +0000

    server running

--- handler.js
+++ handler.js
@@ -1,15 +1,16 @@
 "use strict";
 
 module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+  console.log('hello')
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify(
+  //     {
+  //       message: "hello",
+  //       input: event,
+  //     },
+  //     null,
+  //     2
+  //   ),
+  // };
 };
